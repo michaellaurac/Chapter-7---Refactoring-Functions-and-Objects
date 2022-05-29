ac99b17350a1fee06b501dc064e08fef7151a741
Michaël L <michaellaurac@gmail.com>
2022-05-29 21:53:21 +0200

    Merging chordCountsInLabels and probabilityOfChordsInLabels to the first reference

--- nb.js
+++ nb.js
@@ -4,10 +4,9 @@ const classifier = {
   labelCounts: new Map(),
   labelProbabilities: new Map(),
   chordCountsInLabels: new Map(),
-  probabilityOfChordsInLabels: new Map(),
   smoothing: 1.01,
   valueForChordDifficulty: function (difficulty, chord) {
-    const value = this.probabilityOfChordsInLabels.get(difficulty)[chord];
+    const value = this.chordCountsInLabels.get(difficulty)[chord];
     return value ? value + this.smoothing : 1;
   },
   classify: function (chords) {
@@ -73,10 +72,9 @@ function setChordCountsInLabels () {
 };
 
 function setProbabilityOfChordsInLabels () {
-  classifier.probabilityOfChordsInLabels = classifier.chordCountsInLabels;
-  classifier.probabilityOfChordsInLabels.forEach(function (_chords, difficulty) {
-    Object.keys(classifier.probabilityOfChordsInLabels.get(difficulty)).forEach(function (chord) {
-      classifier.probabilityOfChordsInLabels.get(difficulty)[chord] /= classifier.songs.length;
+  classifier.chordCountsInLabels.forEach(function (_chords, difficulty) {
+    Object.keys(classifier.chordCountsInLabels.get(difficulty)).forEach(function (chord) {
+      classifier.chordCountsInLabels.get(difficulty)[chord] /= classifier.songs.length;
     });
   });
 };
