01979984d950d35a19b469109d745c2e6ed550ea
Michaël L <michaellaurac@gmail.com>
2022-05-29 21:20:36 +0200

    Replace the 1st occurence of classifier by this and the next 2 by self set to this in the classify function

--- nb.js
+++ nb.js
@@ -8,15 +8,16 @@ const classifier = {
   classify: function (chords) {
     const smoothing = 1.01;
     const classified = new Map();
-    classifier.labelProbabilities.forEach(function (_probabilities, difficulty) {
+    const self = this;
+    this.labelProbabilities.forEach(function (_probabilities, difficulty) {
       const totalLikelihood = chords.reduce(function (total, chord) {
-        const probabilityOfChordInLabel = classifier.probabilityOfChordsInLabels.get(difficulty)[chord];
+        const probabilityOfChordInLabel = self.probabilityOfChordsInLabels.get(difficulty)[chord];
         if (probabilityOfChordInLabel) {
           return total * (probabilityOfChordInLabel + smoothing);
         } else {
           return total;
         }
-      }, classifier.labelProbabilities.get(difficulty) + smoothing);
+      }, self.labelProbabilities.get(difficulty) + smoothing);
       classified.set(difficulty, totalLikelihood);
     });
     return classified;
